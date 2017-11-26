bba7d65fc43f04832baf19fd75ef708d36a42490
Pradeep Kumar <pradeep@keepworks.com>
2017-11-26 21:14:06 +0530

    Working

---
+++ handlers_test.go
@@ -0,0 +1,28 @@
+package main_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	. "github.com/RabidFire/fooproject"
+)
+
+func TestRootHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	http.HandlerFunc(RootHandler).ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Error("expected status 200, got:", rr.Code)
+	}
+
+	if rr.Body.String() != "Hello World" {
+		t.Error("expected body Hello World, got:", rr.Body.String())
+	}
+}
